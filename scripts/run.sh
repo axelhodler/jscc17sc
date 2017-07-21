5684801534e2aa629702e14237b2d3e275142f1d
Axel Hodler <axel@hodler.co>
2017-07-21 09:17:58 +0200

    make sure the build is present

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+./node_modules/truffle/build/cli.bundled.js compile
 ./node_modules/truffle/build/cli.bundled.js migrate
 node ./scripts/prepare_web_app.js
 ./node_modules/http-server/bin/http-server ui
